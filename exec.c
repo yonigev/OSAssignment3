b6faa56b6307460768d2550569a1f18557f53f26
yonig <yonigev@gmail.com>
2018-05-19 14:15:05 +0300

    a

--- exec.c
+++ exec.c
@@ -52,37 +52,30 @@ exec(char *path, char **argv) {
             goto bad;
         if (ph.vaddr + ph.memsz < ph.vaddr)
             goto bad;
-       #ifndef NONE
-            //added task1
-            int current_in_ram  =   numOfPagedIn(curproc);                          //number of current pages in ram
-            int toAdd           =   (ph.vaddr+ph.memsz - sz)/PGSIZE;                 //number of pages we want to add
-            int to_page_out     =   current_in_ram +toAdd - MAX_PSYC_PAGES;         //how many to page out (make room)
-            if(to_page_out  >   0)
-                if(page_out_N(curproc, to_page_out)!= to_page_out)
-                    goto bad;
-            int oldsz=sz;
-            if ((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
+        //added task1
+        int current_in_ram  =   numOfPagedIn(curproc);                          //number of current pages in ram
+        int toAdd           =   (ph.vaddr+ph.memsz - sz)/PGSIZE;                 //number of pages we want to add
+        int to_page_out     =   current_in_ram +toAdd - MAX_PSYC_PAGES;         //how many to page out (make room)
+        if(to_page_out  >   0)
+            if(page_out_N(curproc, to_page_out)!= to_page_out)
                 goto bad;
-            int newsz=sz;
-            //while allocuvm succeeded -
-            cprintf("is_user_proc: %d\n",is_user_proc(curproc));
-            if(is_user_proc(curproc) && sz > 0){
-                while ( oldsz < newsz){
-                    if(curproc!=0)
-                        cprintf("proc id- %d\n",curproc->pid);
-                    cprintf("adding total - %d\n",(newsz-oldsz));
-                    cprintf("adding new page - %d\n",oldsz);
-                    add_new_page(curproc,(void *)PGROUNDUP(oldsz));
-                    cprintf("added new page - %d\n",oldsz);
-                    oldsz+=PGSIZE;
-                }
-            }
-        #endif
-        //if NONE! act normal.
-        #ifdef NONE
+        int oldsz=sz;
         if ((sz = allocuvm(pgdir, sz, ph.vaddr + ph.memsz)) == 0)
             goto bad;
-        #endif
+        int newsz=sz;
+        //while allocuvm succeeded -
+        cprintf("is_user_proc: %d\n",is_user_proc(curproc));
+        if(is_user_proc(curproc) && sz > 0){
+            while ( oldsz < newsz){
+                if(curproc!=0)
+                    cprintf("proc id- %d\n",curproc->pid);
+                cprintf("adding total - %d\n",(newsz-oldsz));
+                cprintf("adding new page - %d\n",oldsz);
+                add_new_page(curproc,(void *)PGROUNDUP(oldsz));
+                cprintf("added new page - %d\n",oldsz);
+                oldsz+=PGSIZE;
+            }
+        }
 
         if (ph.vaddr % PGSIZE != 0)
             goto bad;
