4b4c29ad1c06ccfe7d7a7eed7e17d12755a1d7f5
yonig <yonigev@gmail.com>
2018-05-19 13:07:10 +0300

    a

--- proc.c
+++ proc.c
@@ -22,6 +22,7 @@ static void wakeup1(void *chan);
 
 int is_user_proc(struct proc* p){
   if(p != 0){
+    cprintf(" is user proc- comparint init to %s\n",p->name);
     if(strncmp(p->name,  "init",4)!=0 && strncmp(p->name,  "sh",2)!=0 )
       return 1;
   }
