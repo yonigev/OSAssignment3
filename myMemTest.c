d5fceea8039a5a8ea0108c31742cb639bc675954
yonig <yonigev@gmail.com>
2018-05-20 16:14:31 +0300

    added myMemTest

---
+++ myMemTest.c
@@ -0,0 +1,31 @@
+#define PGSIZE 4096
+#include "types.h"
+#include "user.h"
+#include "stat.h"
+int main(){
+
+    char arr1[PGSIZE]={1};
+    char arr2[2*PGSIZE]={2};
+
+    int i;
+    for(i=0; i<PGSIZE; i++){
+        printf(1, "%c",arr1[i]);
+    }
+    
+    for(i=0; i<PGSIZE * 2; i++){
+        printf(1, "%c",arr2[i]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
