e0502c6db313673e40973e45ca42132e4f58c42c
yonig <yonigev@gmail.com>
2018-05-20 16:51:40 +0300

    added exit to test

--- myMemTest.c
+++ myMemTest.c
@@ -4,14 +4,24 @@
 #include "stat.h"
 int main(){
 
-    // `
+    char arr1[PGSIZE]={1};
+    char arr2[2*PGSIZE]={2};
+
+    int i;
+    for(i=0; i<PGSIZE; i++){
+        printf(1, "%c",arr1[i]);
+    }
+    
+    for(i=0; i<PGSIZE * 2; i++){
+        printf(1, "%c",arr2[i]);
+    }
 
 
 
 
 
 
-
+    exit();
 
 
 
