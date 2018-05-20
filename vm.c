ca40732f25abbdfedd06e0e5b6677bb6dd2964e6
yonig <yonigev@gmail.com>
2018-05-20 11:31:04 +0300

    forgot #ifdefs

--- vm.c
+++ vm.c
@@ -803,7 +803,7 @@ void*
 select_page_to_back(struct proc *p){
   //implement algorithms
   
-  //#ifdef NFUA
+  #ifdef NFUA
   struct page  min_page={.exists = 0, .vaddr=(void *)0, .in_back=0,.offset=0, .age=0,.age2=0};     //vaddr of that page
   int i = 0;
   struct page * pa  =   (&(p->paging_meta))->pages;
@@ -823,10 +823,10 @@ select_page_to_back(struct proc *p){
   }
   //return this page's vaddr
   return min_page.vaddr;
-  //#endif
+  #endif
 
 
-  //#ifdef LAPA
+  #ifdef LAPA
   int    min_count;         //min num of set bits
   struct page  min_page;     //vaddr of that page
   int i = 0;
@@ -856,7 +856,7 @@ select_page_to_back(struct proc *p){
   }
   //return this page's vaddr
   return min_page.vaddr;
-  //#endif
+  #endif
   #ifdef SCFIFO
 
 
