298d6c533a4476b364a7151622794cd6eda8b879
yonig <yonigev@gmail.com>
2018-05-20 21:45:00 +0300

    calling kfree restored

--- vm.c
+++ vm.c
@@ -666,16 +666,18 @@ addPageToBack(struct proc *p, void* vaddr){
 //page out a page with the adderss vaddr
 int
 pageOut(struct proc *p,void* vaddr){
-  //char* to_free;
+  char* to_free;
 
     cprintf("pageOut: paging out vaddr: %x\n",vaddr);
    //write page to the Back file.
   if(addPageToBack(p,vaddr)){
-    //pte_t *pte=walkpgdir(p->pgdir,vaddr,0);
-    //to_free=(char*)P2V(PTE_ADDR(*pte));   //TODO: why U flag always 0
-    // if(!(to_free=uva2ka(p->pgdir,vaddr)))
-    //   panic("page-out");
-    //kfree(to_free);                                   //free the PHYSICAL memory of the page
+    pte_t *pte=walkpgdir(p->pgdir,vaddr,0);
+    to_free=(char*)P2V(PTE_ADDR(*pte));   //TODO: why U flag always 0
+     if(!(to_free=uva2ka(p->pgdir,vaddr)))
+       panic("page-out");
+    cprintf("pageout - calling kfree\n");
+    kfree(to_free);                                   //free the PHYSICAL memory of the page
+    cprintf("pageout - after kfree\n");
     clearPTE_FLAG(p,vaddr,PTE_P);                     //clear the Present flag from the page table entry
     setPTE_FLAG(p,vaddr,PTE_PG);                      //set the PAGED-OUT flag
     lcr3(V2P(p->pgdir));                              //refresh the Table Lookaside Buffer            
