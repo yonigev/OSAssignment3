990a5c544f8d2e47978b89ed7d83aa6b4be80be7
yonig <yonigev@gmail.com>
2018-05-20 10:26:51 +0300

    g

--- vm.c
+++ vm.c
@@ -417,11 +417,12 @@ struct page dequeue(struct proc *pr) {
     meta=*pr->paging_meta;
     struct page toReturn = meta.pq.pages[0];
     struct page_queue pq=meta.pq;
-    pq.pages[0] = NULL;
+    pq.pages[0].exists  = 0;          //delete
+    pq.pages[0].vaddr   = (void *)0;  //it
     int i;
     for (i = 0; i < MAX_TOTAL_PAGES; i++) {
         if (i == MAX_TOTAL_PAGES - 1){
-            pq.pages[i] = {0,0,0,0,0,0};
+            //pq.pages[i] = {0,0,0,0,0,0};
             pq.pages[i].exists = 0;
             pq.pages[i].vaddr  = 0;
         }
