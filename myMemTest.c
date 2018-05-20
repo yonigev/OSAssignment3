fbb06a09a345c2ef7d3867ec75b409921949edee
yonig <yonigev@gmail.com>
2018-05-20 16:18:18 +0300

    a

--- myMemTest.c
+++ myMemTest.c
@@ -4,17 +4,17 @@
 #include "stat.h"
 int main(){
 
-    char arr1[10]={1};
-    char arr2[20]={2};
+    // char arr1[10]={1};
+    // char arr2[20]={2};
 
-    int i;
-    for(i=0; i<10; i++){
-        printf(1, "%c",arr1[i]);
-    }
+    // int i;
+    // for(i=0; i<10; i++){
+    //     printf(1, "%c",arr1[i]);
+    // }
     
-    for(i=0; i<20 ; i++){
-        printf(1, "%c",arr2[i]);
-    }
+    // for(i=0; i<20 ; i++){
+    //     printf(1, "%c",arr2[i]);
+    // }
 
 
 
