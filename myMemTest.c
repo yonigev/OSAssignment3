1b0b545d3c93d9a20321660e708cc127b4f6ad60
yonig <yonigev@gmail.com>
2018-05-21 20:48:25 +0300

    asdf

--- myMemTest.c
+++ myMemTest.c
@@ -82,7 +82,8 @@ void globalTest(){
 
 int main(int argc, char *argv[]){
   globalTest();			//for testing each policy efficiency
-  forkTest();			//for testing swapping machanism in fork.
+    
+    forkTest();			//for testing swapping machanism in fork.
   // printf(1,"memtest done\n");
   exit();
 }
