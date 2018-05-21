032bbc78514f0e23db793de82b2892a5210ddbb4
yonig <yonigev@gmail.com>
2018-05-21 20:51:58 +0300

    asdf

--- vm.c
+++ vm.c
@@ -385,7 +385,7 @@ copyuvm(pde_t *pgdir, uint sz)
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
-    if(!(*pte & PTE_P))
+    if(!(*pte & PTE_P) && !(*pte & PTE_PG))
       panic("copyuvm: page not present");
     pa = PTE_ADDR(*pte);
     flags = PTE_FLAGS(*pte);
