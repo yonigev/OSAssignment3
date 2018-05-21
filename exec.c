0170a079f87846ad4bfbe710e129ec41528c1791
yonig <yonigev@gmail.com>
2018-05-21 10:29:00 +0300

    a

--- exec.c
+++ exec.c
@@ -43,7 +43,7 @@ exec(char *path, char **argv)
     //create swap file for this execution.
     //if it's not init.
     if(is_user_proc(curproc)){
-        cprintf("creating swapfile for pid- %d\n",curproc->pid);
+        cprintf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2creating swapfile for pid- %d\n",curproc->pid);
         //memset(curproc->paging_meta,0,sizeof(struct p_meta));
         reset_paging_meta(curproc);
         if(createSwapFile(curproc) != 0)
