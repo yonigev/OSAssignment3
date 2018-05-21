212ce1e5a68357cf1485f35526279f39d4355851
yonig <yonigev@gmail.com>
2018-05-21 13:37:31 +0300

    newBuff

--- vm.c
+++ vm.c
@@ -657,7 +657,10 @@ addPageToBack(struct proc *p, void* vaddr){
     if(free_offset == PGFILE_FULL_ERR)
       return 0;
     cprintf("writing to swapfile: %x , %x\n",vaddr, (char *)PTE_ADDR(vaddr));
-    writeToSwapFile(p,(char *)PTE_ADDR(vaddr),free_offset,PGSIZE);    //  write the page to the swap file
+    char* newBuff;
+    pte_t *entry= walkpgdir(p->pgdir,vaddr,0);
+    newBuff=P2V(PTE_ADDR(entry));
+    writeToSwapFile(p,newBuff,free_offset,PGSIZE);    //  write the page to the swap file
     page_out_meta(p,(char *)PTE_ADDR(vaddr),free_offset);                       //  add to meta-data of the process
    
     return 1;
