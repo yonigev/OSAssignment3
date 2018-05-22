4ccd4097851df8064690994674ab893d31d50eb7
yonig <yonigev@gmail.com>
2018-05-22 09:05:57 +0300

    a

--- myMemTest.c
+++ myMemTest.c
@@ -4,18 +4,39 @@
 #include "fs.h"
 
 #define PGSIZE 4096
-#define ARR_1_SIZE PGSIZE*17
-#define ARR_2_SIZE PGSIZE*17
-
+#define ARR_SIZE PGSIZE*17
 
 /*
 	Test used to check the swapping machanism in fork.
 	Best tested when LIFO is used (for more swaps)
 */
 void forkTest(){
-  // int i;
-  // char * arr;
-  // arr = malloc (ARR_2_SIZE); 
+  int i;
+  char * arr;
+  arr = malloc (ARR_SIZE); //allocates 13 pages (sums to 16), in lifo, OS puts page #15 in file.
+  arr[0]='a';
+  for (i = 0; i < 50; i++) { 
+    arr[49100+i] = 'A'; //last six A's stored in page #16, the rest in #15
+    arr[45200+i] = 'B'; //all B's are stored in page #15.
+  }
+  arr[49100+i] = 0; //for null terminating string...
+  arr[45200+i] = 0;
+    if (fork() == 0){ //is son
+    for (i = 40; i < 50; i++) { 
+	    arr[49100+i] = 'C'; //changes last ten A's to C
+	    arr[45200+i] = 'D'; //changes last ten B's to D
+  	}
+    printf(1, "SON: %s\n",&arr[49100]); // should print AAAAA..CCC...
+    printf(1, "SON: %s\n",&arr[45200]); // should print BBBBB..DDD...
+  	printf(1,"\n");
+    free(arr);
+    exit();
+  } else { //is parent
+    wait();
+    printf(1, "PARENT: %s\n",&arr[49100]); // should print AAAAA...
+    printf(1, "PARENT: %s\n",&arr[45200]); // should print BBBBB...
+    free(arr);
+  }
 
 }
 
@@ -23,7 +44,7 @@ void forkTest(){
 static unsigned long int next = 1;
 int getRandNum() {
   next = next * 1103515245 + 12341;
-  return (unsigned int)(next/65536) % ARR_1_SIZE;
+  return (unsigned int)(next/65536) % ARR_SIZE;
 }
 
 #define PAGE_NUM(addr) ((uint)(addr) & ~0xFFF)
@@ -45,7 +66,7 @@ void globalTest(){
 	char * arr;
 	int i;
 	int randNum;
-	arr = malloc(ARR_1_SIZE); //allocates 14 pages (sums to 17 - to allow more then one swapping in scfifo)
+	arr = malloc(ARR_SIZE); //allocates 14 pages (sums to 17 - to allow more then one swapping in scfifo)
     
 	for (i = 0; i < TEST_POOL; i++) {
 		randNum = getRandNum();	//generates a pseudo random number between 0 and ARR_SIZE
@@ -62,7 +83,7 @@ void globalTest(){
 int main(int argc, char *argv[]){
   globalTest();			//for testing each policy efficiency
     
-  //forkTest();			//for testing swapping machanism in fork.
-  printf(1,"memtest done\n");
+    forkTest();			//for testing swapping machanism in fork.
+  // printf(1,"memtest done\n");
   exit();
 }
